76842fa66ff2be7c8d0acd8017ed919767eb1a29
Malin-Eriksson <sangsta.malin@gmail.com>
2022-10-19 14:14:47 +0200

    form validation started

--- script.js
+++ script.js
@@ -1,27 +1,81 @@
-const name = document.getElementById('name')
-const email = document.getElementById('email')
-const comments = document.getElementById('comments')
-const form = document.getElementById('form')
-const errorElement = document.getElementById('error')
+const onSubmit = event => {
+    event.preventDefault()
+    validate(event)
+}
 
-form.addEventListener('submit', (e) => {
-    let messages = []
-    if (name.value === '' || name.value == null) {
-        messages.push('Name is required')
+const validate = (event) => {
+    switch(event.type) {
+        case 'keyup':
+            validateElement(event.target)
+            break;
+
+        case 'submit':
+            for(let element of event.target)
+                validateElement(element)
+            break;
     }
+}
 
-    if (name.value.length <= 1) {
-        messages.push('Name must be at least 2 characters')
+const validateElement = (element) => {
+    if (element.required) {
+        let label = document.getElementById(`${element.id}-label`).innerText
+        let error = ""
+
+        switch(element.type) {
+            case 'text':
+                if (!isNullOrEmpty(element.value)) {
+                    if (!isMinimumLength(element.value, element.dataset.requiredMin)) {
+                        error = `Your ${label.toLocaleLowerCase()} must contain at least ${element.dataset.requiredMin} letters`
+                    } 
+                } else {
+                    error = `You must enter a ${label.toLocaleLowerCase()}`
+                }
+
+                break;
+            
+                case 'email':
+                    if (!isNullOrEmpty(element.value)) {
+                        if(!isEmailValid(element.value)) {
+                            error = `Your ${label.toLocaleLowerCase()} must be valid (eg. example@domain.com)`
+                        }
+                    } else {
+                        error = `You must enter an ${label.toLocaleLowerCase()}`
+                    }
+                    
+                    break;
+        }
+
+        document.getElementById(`${element.id}-error`).innerText = error
     }
-
-    if (name.value != /^[A-Za-z]+$/) {
-        messages.push('Name can only contain letters')
-    } 
+}
 
 
 
-})
 
 
 
+
+
+
+const isNullOrEmpty = value => {
+    if (value.length === 0)
+        return true
     
+    return false
+}
+
+const isMinimumLength = (value, minLength = 2) => {
+    if (value.length >= minLength)
+        return true
+
+    return false
+}
+
+const isEmailValid = (email) => {
+    const regEx = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+    
+    if (regEx.test(email))
+        return true
+
+    return false
+}
