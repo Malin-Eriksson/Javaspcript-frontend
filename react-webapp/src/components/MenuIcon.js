bc45a869d067cf0a72a8aa64b39e5fc84b1cc2ed
Malin-Eriksson <sangsta.malin@gmail.com>
2022-10-25 16:15:08 +0200

    merge 4

--- react-webapp/src/components/MenuIcon.js
+++ react-webapp/src/components/MenuIcon.js
@@ -1,9 +1,10 @@
 import React from 'react'
 import { NavLink } from 'react-router-dom'
 
-function MenuIcon({link, icon}) {
+function MenuIcon({link, icon, quantity}) {
   return (
     <NavLink className="menu-icon" to={link} end>
+        <span className="position-absolute top-0 start-100 translate-middle badge rounded-pill bg-theme">{quantity}</span>
         <i className={icon}></i>
     </NavLink>
   )
--- react-webapp/src/sections/FooterSection.js
+++ react-webapp/src/sections/FooterSection.js
@@ -5,11 +5,11 @@ const FooterSection = () => {
   return (
     <footer>
         <div className='socialmedia'>
-            <ExternalLinkIcon link="https://sv-se.facebook.com/" icon="fa-brands fa-facebook-f" />
-            <ExternalLinkIcon link="https://www.instagram.com/" icon="fa-brands fa-instagram" />
-            <ExternalLinkIcon link="https://twitter.com/?lang=sv" icon="fa-brands fa-twitter" />
-            <ExternalLinkIcon link="https://www.google.com/" icon="fa-brands fa-google" />
-            <ExternalLinkIcon link="https://se.linkedin.com/" icon="fa-brands fa-linkedin" />
+            <ExternalLinkIcon link="https://facebook.com/" icon="fa-brands fa-facebook-f" />
+            <ExternalLinkIcon link="https://instagram.com/" icon="fa-brands fa-instagram" />
+            <ExternalLinkIcon link="https://twitter.com" icon="fa-brands fa-twitter" />
+            <ExternalLinkIcon link="https://google.com/" icon="fa-brands fa-google" />
+            <ExternalLinkIcon link="https://linkedin.com/" icon="fa-brands fa-linkedin" />
         </div>
         <div>© 2022 Fixxo. All Rights Reserved</div>
 </footer>
