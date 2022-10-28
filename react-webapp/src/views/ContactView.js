c7e3ba09c61d70f92f98f7cc1321ff9114e7a1d7
Malin-Eriksson <sangsta.malin@gmail.com>
2022-10-28 12:15:12 +0200

    contact page started

--- react-webapp/src/views/ContactView.js
+++ react-webapp/src/views/ContactView.js
@@ -1,12 +1,20 @@
 import React from 'react'
 import MainMenuSection from '../sections/MainMenuSection'
 import FooterSection from '../sections/FooterSection'
+import BreadcrumbSection from '../sections/BreadcrumbSection'
+import MapSection from '../sections/MapSection'
+import ContactFormSection from '../sections/ContactFormSection'
 
 function ContactView() {
+window.top.document.title = "Contact | Fixxo."
+
   return (
     <>
       <MainMenuSection />
-      <FooterSection />
+      <BreadcrumbSection currentPage="Contact"/>
+      <MapSection />
+      <ContactFormSection />
+      <FooterSection/>
     </>
 
 
