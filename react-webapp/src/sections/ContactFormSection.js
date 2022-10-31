fd756876aa23c32dd939ae86b08d15732b76b512
Malin-Eriksson <sangsta.malin@gmail.com>
2022-10-31 12:43:41 +0100

    validation added to contact form

--- react-webapp/src/sections/ContactFormSection.js
+++ react-webapp/src/sections/ContactFormSection.js
@@ -1,81 +1,84 @@
 import React, { useState } from 'react'
+import { validate } from '../scripts/validation'
 
-const ContactFormSection = () => {
-    const [contactForm,setContactForm] = useState({name: '', email: '', comment: ''})
-    const [formErrors, setFormErrors] = useState({})
-    const [submitted, setSubmitted] = useState(false)
+const ContactForm = () => {
+  let currentPage = "Contact Us"
+  window.top.document.title = `${currentPage} || Fixxo` 
 
-    const validate = (values) => {
-        const errors = {}
-        const regex_email = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+  const [name, setName] = useState('')
+  const [email, setEmail] = useState('')
+  const [comments, setComments] = useState('')
+  const [errors, setErrors] = useState({})
+  const [submitted, setSubmitted] = useState(false)
 
-        if(!values.name) 
-            errors.name = "You must enter a name"
-        
-        if(!values.email) 
-            errors.email = "You must enter an email address"
-        else if (!regex_email.test(values.email))
-            errors.email = "You must enter an valid email address (eg. name@domain.com)"
+  const handleChange = (e) => {
+    const {id, value} = e.target
 
-
-        if(!values.comment) 
-            errors.comment = "You must enter a comment"
-        else if(values.comment.length < 5)
-            errors.comment = "Your comment must be longer than 5 characters"
-
-        if(Object.keys(errors).length === 0)
-            setSubmitted(true)
-        else
-            setSubmitted(false)
-
-        return errors;
+    switch(id) {
+      case 'name':
+        setName(value)
+        break
+      case 'email':
+        setEmail(value)
+        break
+      case 'comments':
+        setComments(value)
+        break
     }
 
-    const handleChange = (e) => {
-        const {id, value} = e.target
-        setContactForm({...contactForm, [id]: value})
-    }
+    setErrors({...errors, [id]: validate(e)})
+  }
 
-    const handleSubmit = (e) => {
-        e.preventDefault ()
-        setFormErrors(validate(contactForm))
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    setErrors(validate(e, {name, email, comments}))
+  
+    if (errors.name === null && errors.email === null && errors.comments === null) {
+      setSubmitted(true)
+      setName('')
+      setEmail('')
+      setComments('')
+      setErrors({})
+    } else {
+      setSubmitted(false)
     }
+  }
 
 
   return (
-    <section className='contact-form'>
-        <div className='container'>    
-            {
-                submitted ? (<div className='d-flex justify-content-center align-items-center'>
-                        <div>Thank you for your comment!</div>
-                    </div>)
-                    :
-                    (
-                        <>
-                        <h2>Come in Contact with Us</h2>
-                        <form onSubmit={handleSubmit} noValidate>
-                            <div>
-                                <input id='name' type="text" className='error' placeholder="Your Name" value={contactForm.name} onChange={handleChange}/>
-                                <div className='errorMessage'>{formErrors.name}</div>
-                            </div>
-                            <div>
-                                <input id='email' type="email" className='error'placeholder="Your Mail" value={contactForm.email} onChange={handleChange}/>
-                                <div className='errorMessage'>{formErrors.email}</div>
-                            </div>
-                            <div className='textarea'>
-                                <textarea id='comment' className='error' placeholder='Comments' value={contactForm.comment} onChange={handleChange}></textarea>
-                                <div className='errorMessage'>{formErrors.comment}</div>
-                            </div>
-                            <div className='formBtn'>
-                                <button type='submit' className='btn-theme'>Post Comments</button>
-                            </div>
-                        </form>
-                        </>
-                    )
-            }
-        </div>
+    <section className="contact-form mt-5">
+      <div className="container">
+        
+        {
+          submitted ? (
+          <div className="alert alert-success text-center mb-5" role="alert">
+            <h3>Thank you for your comments</h3> 
+            <p>We will contact you as soon as possible.</p>
+            </div>  ) : (<></>)
+        }
+        
+        
+        <h2>Come in Contact with Us</h2>
+        <form onSubmit={handleSubmit} noValidate>
+          <div>
+            <input id="name" className={(errors.name ? 'error': '')} value={name} onChange={handleChange} type="text" placeholder="Your Name" />
+            <div className="errorMessage">{errors.name}</div>
+          </div>
+          <div>
+            <input id="email" className={(errors.email ? 'error': '')} value={email} onChange={handleChange} type="email" placeholder="Your Mail" />
+            <div className="errorMessage">{errors.email}</div>
+          </div>
+          <div className="textarea">
+            <textarea id="comments" className={(errors.comments ? 'error': '')} style={(errors.comments ? {border: '1px solid #FF7373'}: {} )} value={comments} onChange={handleChange} placeholder="Comments"></textarea>
+            <div className="errorMessage">{errors.comments}</div>
+          </div>
+          <div className="formBtn">
+            <button type="submit" className="btn-theme">Post Comments</button>
+          </div>
+        </form>    
+      </div>
     </section>
   )
 }
 
-export default ContactFormSection
+export default ContactForm
--- react-webapp/src/views/ContactView.js
+++ react-webapp/src/views/ContactView.js
@@ -3,7 +3,8 @@ import MainMenuSection from '../sections/MainMenuSection'
 import FooterSection from '../sections/FooterSection'
 import BreadcrumbSection from '../sections/BreadcrumbSection'
 import MapSection from '../sections/MapSection'
-import ContactFormSection from '../sections/ContactFormSection'
+import ContactForm from '../sections/ContactFormSection'
+
 
 function ContactView() {
 window.top.document.title = "Contact | Fixxo."
@@ -13,7 +14,7 @@ window.top.document.title = "Contact | Fixxo."
       <MainMenuSection />
       <BreadcrumbSection currentPage="Contact"/>
       <MapSection />
-      <ContactFormSection />
+      <ContactForm />
       <FooterSection/>
     </>
 
