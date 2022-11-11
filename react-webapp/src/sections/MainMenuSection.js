edd0d98e76393d3047982dc8c60fe2a24cb5545f
Malin-Eriksson <sangsta.malin@gmail.com>
2022-11-11 14:21:19 +0100

    main menu responsive

--- react-webapp/src/sections/MainMenuSection.js
+++ react-webapp/src/sections/MainMenuSection.js
@@ -1,10 +1,16 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { NavLink } from 'react-router-dom'
 import MenuIcon from '../components/MenuIcon'
 import { useShoppingCart } from '../contexts/ShoppingCartContext'
 
 
+
 const MainMenuSection = () => {
+  const [showMenu, setShowMenu] = useState(false)
+
+    const toggleMenu = () => {
+      setShowMenu(!showMenu)
+    }
 
   const { cartQuantity } = useShoppingCart()
   
@@ -12,7 +18,7 @@ const MainMenuSection = () => {
   return (
     <nav className="mainmenu container">
         <NavLink className="logo" to="/" end>Fixxo.</NavLink>
-        <div className="menu-links">
+        <div className={`menu-links ${showMenu ? "d-grid" : "" }`}>
             <NavLink className="menu-link" to="/" end>Home</NavLink>
             <NavLink className="menu-link" to="/categories" end>Categories</NavLink>
             <NavLink className="menu-link" to="/products">Products</NavLink>
@@ -21,11 +27,13 @@ const MainMenuSection = () => {
         <div className="menu-icons">
             <MenuIcon link="/search" icon="fa-regular fa-magnifying-glass" />
             <MenuIcon hideMobile="true" link="/compare" icon="fa-regular fa-code-compare" />
-            <MenuIcon hideMobile="true" quantity="3" link="/wishlist" icon="fa-regular fa-heart" />
+            <MenuIcon quantity="3" link="/wishlist" icon="fa-regular fa-heart" />
             <button className="menu-icon" type="button" data-bs-toggle="offcanvas" data-bs-target="#shoppingCart" aria-controls="shoppingCart">
               <span className="position-absolute top-0 start-100 translate-middle badge rounded-pill bg-theme">{cartQuantity}</span>
               <i className="fa-regular fa-bag-shopping"></i>
             </button>
+
+            <button onClick={toggleMenu} className='d-xl-none btn-menu-icon menu-icon'><i className='fa-regular fa-bars'></i></button>
         </div>
     </nav>
   )
